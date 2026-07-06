6d11847537a04675d6b9896222ecc5966591e7dd
Abhishek Jha <iamabhishek1310@gmail.com>
2026-07-06 23:26:44 +0530

    Added SMTP server with nodemailer

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,3 +3,5 @@ export const CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string
 export const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
 export const REDIRECT_URI = process.env.GOOGLE_CALLBACK_URL as string
 export const DATABASE_URL = process.env.DATABASE_URL as string
+export const SMTP_EMAIL_USER = process.env.SMTP_EMAIL_USER as string
+export const SMTP_EMAIL_PASS = process.env.SMTP_EMAIL_PASS as string
--- src/infrastructure/email/sendEmail.ts
+++ src/infrastructure/email/sendEmail.ts
@@ -1,9 +1,37 @@
-import type { Request, Response } from "express";
+import { Request, Response } from "express";
+import transporter from "./index"
+import { SMTP_EMAIL_USER } from "../../config/env";
+import nodemailer from "nodemailer"
 
-export const sendEmail = async (req: Request, res: Response) => {
+export const sendInviteEmail = async (email: string, workspaceName: string, inviteLink: string) => {
     try {
+        const msg = await transporter.sendMail({
+            from: SMTP_EMAIL_USER,
+            to: email,
+            subject: "Invitation to workspace from dropdesk",
+            html: `
+      <p>You have been invited to join the workspace "${workspaceName}".</p>
+      <p>Click the link below to accept the invitation:</p>
+      <a href="${inviteLink}">Accept Invitation</a>
+
+
+      <br/>
+
+      <p> made withㅤꨄ︎ by Abhishek</p>
+      <br/>
+    `
+
+        })
+
+        console.log("Message sent: %s", msg.messageId);
+        // Preview URL is only available when using an Ethereal test account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(msg));
 
     } catch (error) {
-
+        console.error("Error while sending mail:", error);
     }
+
 }
+
+
+// thanks to nodemailer doc ..
