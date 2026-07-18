34f2f011721b28afa67c6ca064a2e6bcb5406860
Abhishek Jha <iamabhishek1310@gmail.com>
2026-07-18 21:28:05 +0530

    Added redis rate limiter for auth and globals

--- src/modules/oauth/oauth.routes.ts
+++ src/modules/oauth/oauth.routes.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import { getGoogleCallback, getGoogleOAuthUrl, getUser } from "./oauth.service"
 import { requireAuth } from "../../api/middlewares/auth"
+import { authLimiter } from "../../infrastructure/rate-limiter"
 
 const router = express.Router()
 
-router.get("/google", getGoogleOAuthUrl)
+router.get("/google", authLimiter, getGoogleOAuthUrl)
 router.get("/callback", getGoogleCallback)
 router.get("/me", requireAuth, getUser)
+//todo: save the /me user to redis with hset
 
 export default router
