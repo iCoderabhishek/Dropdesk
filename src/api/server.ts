790b1f0672571e8f67a104695b1f09970ac6da0b
Abhishek Jha <iamabhishek1310@gmail.com>
2026-07-28 23:15:35 +0530

    feat: initialize backend server with CORS, environment configuration, and FileVersion database schema

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import express from "express"
-import { PORT } from "../config/env"
+import { FRONTEND_BASE_URL, PORT } from "../config/env"
 import oauthRoutes from "../modules/oauth/oauth.routes"
 import workspaceRoutes from "../modules/workspaces/workspace.routes"
 import cookieSession from "cookie-session"
@@ -8,11 +8,16 @@ import filesRoutes from "../modules/media/media.routes"
 import "../workers/export.worker"
 import "../workers/gc.worker"
 import limiter from "../infrastructure/rate-limiter"
+import cors from "cors"
 dotenv.config()
 
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: FRONTEND_BASE_URL,
+    credentials: true
+}))
 
 // app middlewares
 app.use(cookieSession({
